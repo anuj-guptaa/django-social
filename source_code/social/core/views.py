d60c71c52efceb1f8e41bb93507b791729d3d868
Anuj Gupta <anujgupta@windowslive.com>
2023-10-15 17:08:25 +0700

    add settings page

--- source_code/social/core/views.py
+++ source_code/social/core/views.py
@@ -12,7 +12,33 @@ def index(request):
 
 @login_required(login_url="signin")
 def settings(request):
-  return render(request, 'setting.html')
+  user_profile = Profile.objects.get(user=request.user)
+
+  if request.method == "POST":
+    if request.FILES.get('image') == None:
+      image = user_profile.profile_img
+      bio = request.POST['bio']
+      location = request.POST['location']
+
+      user_profile.profile_img = image
+      user_profile.bio = bio
+      user_profile.location = location
+      user_profile.save()
+
+    if request.FILES.get('image') != None:
+      image = request.FILES.get('image')
+      bio = request.POST['bio']
+      location = request.POST['location']
+
+      user_profile.profile_img = image
+      user_profile.bio = bio
+      user_profile.location = location
+      user_profile.save()
+
+    return redirect('settings')
+
+  else:
+    return render(request, 'setting.html', {'user_profile': user_profile})
 
 def signup(request):
 
@@ -34,12 +60,14 @@ def signup(request):
         user.save()
 
         #Log user in and redirect to settings page
+        user_login = auth.authenticate(username=username, password=password)
+        auth.login(request, user_login)
 
         #create a Profile object for new user
         user_model = User.objects.get(username=username)
         new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
         new_profile.save()
-        return redirect('signup')
+        return redirect('settings')
     else:
       messages.info(request, 'Passwords do not match')
       return redirect('signup')
